feb2be30233beb82f5398b07c763e2a84f537624
dchase87 <david.chase4@gmail.com>
2017-04-30 14:22:57 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def alphebetize(word)
+    ordered_array = word.split(//).sort
+    ordered_string = ordered_array.join('')
+  end
+
+  def match(array)
+    my_word = alphebetize(@word)
+    match_array = []
+
+    array.each do |word|
+      if alphebetize(word) == my_word
+        match_array << word
+      end
+    end
+    match_array
+  end
+end
